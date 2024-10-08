da159d778d667aa108c7d7eb86dfb2bf823af49c
BHIN <vksek375@gmail.com>
2024-10-08 23:28:19 +0900

    스나이퍼용 빌딩맵 추가

--- Client/Code/Stage.cpp
+++ Client/Code/Stage.cpp
@@ -519,7 +519,7 @@ CLayer* CStage::Find_Layer(const _tchar* _pLayerTag)
 
 void CStage::MapLoad2(CLayer* _pLayer, CLayer* _pLayer2, CLayer* _pLayer3)
 {
-	HANDLE		hFile = CreateFile(L"../Data/BossStage.txt",	// 파일 이름까지 포함된 경로
+	HANDLE		hFile = CreateFile(L"../Data/BuildingStage.txt",	// 파일 이름까지 포함된 경로
 		GENERIC_READ,		// 파일 접근 모드(GENERIC_WRITE : 쓰기, GENERIC_READ : 읽기)
 		NULL,				// 공유 방식(파일이 열려 있는 상태에서 다른 프로세스가 오픈 할 때 허가 할 것인가)
 		NULL,				// 보안 속성
